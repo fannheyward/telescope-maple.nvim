55e7b6a70f8fd3133e52e04d3a25aff47a11df71
Heyward Fann <fannheyward@gmail.com>
2022-01-13 20:40:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# telescope-maple.nvim
+
+<!-- markdownlint-disable no-inline-html -->
+<a href="https://github.com/sponsors/fannheyward"><img src="https://user-images.githubusercontent.com/345274/133218454-014a4101-b36a-48c6-a1f6-342881974938.png" alt="GitHub Sponsors" /></a>
+<a href="https://patreon.com/fannheyward"><img src="https://c5.patreon.com/external/logo/become_a_patron_button.png" alt="Patreon donate button" /></a>
+<a href="https://paypal.me/fannheyward"><img src="https://user-images.githubusercontent.com/345274/104303610-41149f00-5505-11eb-88b2-5a95c53187b4.png" alt="PayPal donate button" /></a>
+
+An extension for [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) to grep from [maple](https://github.com/liuchengxu/vim-clap/tree/master/crates/maple_cli).
+
+## Get Started
+
+```viml
+Plug 'fannheyward/telescope-maple.nvim'
+
+...
+require('telescope').load_extension('maple')
+```
+
+## Usage
+
+`:Telescope maple`
+
+## License
+
+MIT
---
+++ lua/telescope/_extensions/maple.lua
@@ -0,0 +1,72 @@
+local conf = require('telescope.config').values
+local utils = require "telescope.utils"
+local finders = require('telescope.finders')
+local sorters = require "telescope.sorters"
+local pickers = require('telescope.pickers')
+
+---@diagnostic disable-next-line: undefined-global
+local vim = vim
+
+local maple = function(opts)
+  local make_display = function(filename, text)
+    local display, hl_group = utils.transform_devicons(filename, text)
+
+    if hl_group then
+      return display, { { { 1, 5 }, hl_group } }
+    else
+      return display
+    end
+  end
+
+  local maple_finder = finders.new_job(
+    function(prompt)
+      if not prompt or prompt == "" then
+        return
+      end
+
+      return {'maple', 'grep', prompt}
+    end,
+
+    function(entry)
+      if not entry or entry == "" then
+        return
+      end
+
+      local text = vim.json.decode(entry).text
+      if not text or text == "" then
+        return
+      end
+
+      local parts = vim.split(text, ':')
+      return {
+        valid = true,
+        path = parts[1],
+        filename = parts[1],
+        lnum = tonumber(parts[2]),
+        col = tonumber(parts[3]),
+        text = parts[4],
+        value = text,
+        ordinal = text,
+        display = make_display(parts[1], text),
+      }
+    end,
+
+    opts.max_results,
+    opts.cwd
+  )
+
+  pickers.new(opts, {
+    prompt_title = "maple grep",
+    previewer = conf.grep_previewer(opts),
+    sorter = sorters.highlighter_only(opts),
+    finder = maple_finder,
+  }):find()
+end
+
+return require('telescope').register_extension({
+  exports = {
+    maple = maple,
+  },
+})
+
+-- vim: set sw=2 ts=2 sts=2 et
